ee930e073e0fb4b20a316e56240e0a1a1d7ff5d0
Aara Amuthan <aaraamuthanb@gmail.com>
2024-03-19 08:40:40 +0530

    adding projects section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,66 +5,88 @@ import List from "../../components/List/List";
 import data from "../../assets/data/data.json";
 import localStyles from "./Home.module.css";
 import useClassName from "../../hooks/useClassName";
+import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
 
 const Home = () => {
-    const cx = useClassName(localStyles);
-    return (
-        <Page>
-            <Hero />
-            <Card>
-                <h3>About</h3>
-                <p>{data.about}</p>
-            </Card>
-            <Card>
-                <h3>Experience</h3>
-                <List>
-                    {data.experiences.map((experience) => (
-                        <li key={experience.id} className={cx("row-layout")}>
-                            <img
-                                className={cx("row-image")}
-                                src={experience.companyLogo}
-                                alt={`${experience.companyName} Logo`}
-                            />
-                            <div className={cx("row-details")}>
-                                <h4>{experience.position}</h4>
-                                <p>
-                                    {experience.companyName} .{" "}
-                                    {experience.employmentType}
-                                </p>
-                                <p>
-                                    {experience.from} - {experience.to} .{" "}
-                                    {experience.noOfYears}
-                                </p>
-                            </div>
-                        </li>
-                    ))}
-                </List>
-            </Card>
-            <Card>
-                <h3>Education</h3>
-                <List>
-                    {data.educations.map((education) => (
-                        <li key={education.id} className={cx("row-layout")}>
-                            <img
-                                className={cx("row-image")}
-                                src={education.instituteLogo}
-                                alt={`${education.instituteName} Logo`}
-                            />
-                            <div className={cx("row-details")}>
-                                <h4>{education.instituteName}</h4>
-                                <p>
-                                    {education.program} . {education.percentage}
-                                </p>
-                                <p>
-                                    {education.from} - {education.to}
-                                </p>
-                            </div>
-                        </li>
-                    ))}
-                </List>
-            </Card>
-        </Page>
-    );
+  const cx = useClassName(localStyles);
+  return (
+    <Page>
+      <Hero />
+      <Card>
+        <h3>About</h3>
+        <p>{data.about}</p>
+      </Card>
+      <Card>
+        <h3>Experience</h3>
+        <List>
+          {data.experiences.map((experience) => (
+            <li key={experience.id} className={cx("row-layout")}>
+              <img
+                className={cx("row-image")}
+                src={experience.companyLogo}
+                alt={`${experience.companyName} Logo`}
+              />
+              <div className={cx("row-details")}>
+                <h4>{experience.position}</h4>
+                <p>
+                  {experience.companyName} . {experience.employmentType}
+                </p>
+                <p>
+                  {experience.from} - {experience.to} . {experience.noOfYears}
+                </p>
+              </div>
+            </li>
+          ))}
+        </List>
+      </Card>
+      <Card>
+        <h3>Education</h3>
+        <List>
+          {data.educations.map((education) => (
+            <li key={education.id} className={cx("row-layout")}>
+              <img
+                className={cx("row-image")}
+                src={education.instituteLogo}
+                alt={`${education.instituteName} Logo`}
+              />
+              <div className={cx("row-details")}>
+                <h4>{education.instituteName}</h4>
+                <p>
+                  {education.program} . {education.percentage}
+                </p>
+                <p>
+                  {education.from} - {education.to}
+                </p>
+              </div>
+            </li>
+          ))}
+        </List>
+      </Card>
+      <Card>
+        <h3>Projects</h3>
+        <List>
+          {data.projects.map((project) => (
+            <li key={project.id} className={cx("projects-row")}>
+              <div className={cx("justify-between")}>
+                <h4>{project.title}</h4>
+                <div className={cx("row-layout")}>
+                  <a href={`../${project.id}`}>
+                    <BsBoxArrowUpRight size={20} />
+                  </a>
+                  <a href={`${project.githubUrl}`} target="_blank">
+                    <BsGithub size={20} />
+                  </a>
+                </div>
+              </div>
+              <div>
+                <p>{project.description}</p>
+              </div>
+            </li>
+          ))}
+        </List>
+      </Card>
+    </Page>
+  );
 };
 
 export default Home;
